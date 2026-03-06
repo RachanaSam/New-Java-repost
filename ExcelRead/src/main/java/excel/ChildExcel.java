3701e8a5e404a13b6d0953eebc7f0395108004a2
RACHANA <RACHANA@LAPTOP-R29MUJG7>
2026-03-06 10:13:54 +0000

    Updated ChildExcel.java logic

--- ExcelRead/src/main/java/excel/ChildExcel.java
+++ ExcelRead/src/main/java/excel/ChildExcel.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 public class ChildExcel {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println(ParentExcel.getStringData(1, 1));
+		//System.out.println(ParentExcel.getStringData(1, 1));
 		System.out.println(ParentExcel.getintData(1, 1));
-		System.out.println(ParentExcel.getfloatData(3, 1));
+		//System.out.println(ParentExcel.getfloatData(3, 1));
 		
 
 	}
